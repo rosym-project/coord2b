60f9efc7d989c34b28755eade13aca5b9ef35e2f
Vamsi Kalagaturu <vamsikalagaturu@gmail.com>
2026-06-24 21:11:22 +0200

    Use stdbool for event loop booleans
    event_loop.h was defining true and false itself for C consumers while only including <stdbool.h> for C++ builds. Include the standard header unconditionally so boolean macros come from the C library instead of this project header.

--- include/coord2b/types/event_loop.h
+++ include/coord2b/types/event_loop.h
@@ -1,17 +1,10 @@
 #ifndef COORD2B_TYPES_EVENT_LOOP_H
 #define COORD2B_TYPES_EVENT_LOOP_H
 
-#ifdef __cplusplus
 #include <stdbool.h>
+
+#ifdef __cplusplus
 extern "C" {
-#endif
-
-#ifndef true
-#define true 1
-#endif
-
-#ifndef false
-#define false 0
 #endif
 
 struct events {
